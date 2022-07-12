a437fc952f3e574c479eb35ae4d955f3294aa708
rodrigopimentel <rodrigopimentel@uri.edu>
2022-07-12 19:05:48 -0400

    js complete; add README

--- main.js
+++ main.js
@@ -13,10 +13,70 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Returns an object representing P. aequor
+const pAequorFactory = (specimenNum, dna) => {
+  return {
 
+    // properties
+    _specimenNum: specimenNum,
+    _dna: dna,
 
+    // methods
 
+    // randomly mutates a base and returns array of dna bases
+    mutate() {
+      randIdx = Math.floor((Math.random() * 15));
+      newBase = returnRandBase();
+      if (this._dna[randIdx] != newBase) {
+        this._dna[randIdx] = newBase;
+      } else {
+        this.mutate();
+      }
+      return this._dna;
+    },
 
+    // returns true if 60% or more of bases or Cs or Gs, false otherwise
+    willLikelySurvive() {
+      let cOrGCount = 0;
+      this._dna.forEach(base => {
+        if (base === 'C' || base === 'G') {
+          cOrGCount++;
+        }
+      });
+      
+      if ((cOrGCount / 15) >= 0.60) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  
+    // compares DNA with another object representing P. aequor and prints out % shared
+    compareDNA(pAequorObj) {
+      let dnaCommonCount = 0;
+      for (let i = 0; i < 15; i++) {
+        if (this._dna[i] === pAequorObj._dna[i]) {
+          dnaCommonCount++;
+        }
+      }
+      console.log(`specimen ${this._specimenNum} and specimen ${pAequorObj._specimenNum} have ${Math.floor((dnaCommonCount / 15) * 100)}% DNA in common`);
+    }
+  }
+};
 
+// makes a set number of pAequor objects that are likely to survive
+const makeFitPAequor = n => {
+  let fitPAequors = [];
+  let fitCount = 0;
+  let tempPAequor;
+  while (fitCount < n) {
+    tempPAequor = pAequorFactory(fitCount, mockUpStrand());
+    if (tempPAequor.willLikelySurvive()) {
+      fitPAequors.push(tempPAequor);
+      fitCount++;
+    }
+  }
+  return fitPAequors;
+};
 
-
+console.log(makeFitPAequor(30));
